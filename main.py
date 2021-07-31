9aaaf9858d319d294bd96aabe5d8085bad70b832
hemangi-coder <81115378+hemangi-coder@users.noreply.github.com>
2021-07-31 22:07:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import streamlit as st
-import pandas as pd
-from PIL import Image
+from webcam import webcam
 
-@st.cache
-def get_data():
-    path = r'StudentNamesList.csv'
-    return pd.read_csv(path)
-df = get_data()
+captured_image = webcam()
+if captured_image is None:
+    st.write("Waiting for capture...")
+else:
+    st.write("Got an image from the webcam:")
+    st.image(captured_image)
 
-names = df['Student Name'].drop_duplicates()
-name_choice = st.selectbox('Select your Name:', names)
-
-subject = df['Subject'].drop_duplicates()
-subject_choice = st.selectbox('Select your Subject:', subject)
-
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+#streamlit-webcam-example-0.1.0
+#pyarrow
